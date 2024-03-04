72ea8d12176f90e6919ee59468f5b5d12699fcc9
Tarik <tarik.n@pcgamingrace.com>
2024-03-04 04:01:25 +0100

    Centered response and adjusted padding

--- components/ui/card.tsx
+++ components/ui/card.tsx
@@ -9,7 +9,7 @@ const Card = React.forwardRef<
   <div
     ref={ref}
     className={cn(
-      "rounded-lg border bg-card text-card-foreground shadow-sm",
+      "rounded-lg border bg-card text-card-foreground shadow-sm flex items-center justify-center",
       className
     )}
     {...props}
@@ -60,7 +60,7 @@ const CardContent = React.forwardRef<
   HTMLDivElement,
   React.HTMLAttributes<HTMLDivElement>
 >(({ className, ...props }, ref) => (
-  <div ref={ref} className={cn("p-6 pt-0", className)} {...props} />
+  <div ref={ref} className={cn("p-2", className)} {...props} />
 ))
 CardContent.displayName = "CardContent"
 
